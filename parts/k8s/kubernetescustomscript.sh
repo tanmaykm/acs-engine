539c160bc6374d9434f6dfdcfdc0f03cb8e2e212
Pradeep Mudlapur <pradeep@juliacomputing.com>
2018-12-11 14:31:53 +0530

    Fix custom extension script issue

--- parts/k8s/kubernetescustomscript.sh
+++ parts/k8s/kubernetescustomscript.sh
@@ -473,7 +473,6 @@ fi
 
 ephemeralDocker
 installDocker
-runAptDaily
 configureK8s
 ensureDocker
 configNetworkPlugin
@@ -531,4 +530,6 @@ if $REBOOTREQUIRED; then
   # wait 1 minute to restart node, so that the custom script extension can complete
   echo 'reboot required, rebooting node in 1 minute'
   /bin/bash -c "shutdown -r 1 &"
+else
+  runAptDaily &
 fi
